8313c2a284db6c6391a921daec812ad856bd05fe
kalema1 <kalemar8@gmail.com>
2024-06-11 16:22:42 +0300

    add button to home page to view all properties
    - added a button to link to the all properties page.
    - added styles to the jsx.

--- components/homeProperties/HomeProperties.jsx
+++ components/homeProperties/HomeProperties.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import properties from "@/properties.json";
 import PropertyCard from "../propertyCard/PropertyCard";
 
@@ -8,18 +9,29 @@ export default function HomeProperties() {
     .slice(0, 3);
 
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
-          Recent Properties
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentroperties.length === 0 && <div>No Property Found</div>}
-          {recentroperties.map((property) => (
-            <PropertyCard property={property} key={property._id} />
-          ))}
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+            Recent Properties
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {recentroperties.length === 0 && <div>No Property Found</div>}
+            {recentroperties.map((property) => (
+              <PropertyCard property={property} key={property._id} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <section className="m-auto max-w-lg my-10 px-6">
+        <Link
+          href="/properties"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Properties
+        </Link>
+      </section>
+    </>
   );
 }
