b3b38b472ae991d88a90d901399056242d2b457a
kalema1 <kalemar8@gmail.com>
2024-06-11 16:56:40 +0300

    create a custom loader for the application
    - created a loading.jsx file for the page loading.
    - created the Loader.jsx file to contain the loader spinner.
    - added react-spinner to the Loader.jsx file.
    - added styles to the Loader component.
    - added the Loader component to the loading page file.

---
+++ app/loading.jsx
@@ -0,0 +1,9 @@
+import Loader from "@/ui/Loader";
+
+export default function LoadingPage() {
+  return (
+    <div>
+      <Loader />
+    </div>
+  );
+}
---
+++ ui/Loader.jsx
@@ -0,0 +1,21 @@
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+export default function Loader({ loading }) {
+  return (
+    <div>
+      <ClipLoader
+        color="#3b82f6"
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+      />
+    </div>
+  );
+}
