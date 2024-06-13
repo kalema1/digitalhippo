c1534c35f936f432c0b936f51c9e6c72d34a94bb
kalema1 <kalemar8@gmail.com>
2024-06-13 14:09:33 +0300

    remove the connection test from home page
    - removed the database connection test from the home page.
    - removed the server test console.

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,8 @@
 import Hero from "@/components/hero/Hero";
 import HomeProperties from "@/components/homeProperties/HomeProperties";
 import InformationBoxes from "@/components/infoBoxes/InformationBoxes";
-import conncetDB from "@/config/database";
 
-export default async function Home() {
-  await conncetDB();
-  console.log("server side");
+export default function Home() {
   return (
     <>
       <Hero />
