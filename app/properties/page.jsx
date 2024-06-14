9b2cfc03ec768f013404687381d159aa096017d0
kalema1 <kalemar8@gmail.com>
2024-06-14 22:13:10 +0300

    fetch data from properties api endpoint
    - created the apiProperties.js file. To contain functions related to
      properties api endpoint.
    - crested a function called fetchProperties. To fetch properties data
      from the properties api endpoint.
    - imported the fetchProperties in the PropertiesPage.
    - replaced the sample data from properties.json file with data from
      database.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,9 @@
 import PropertyCard from "@/components/propertyCard/PropertyCard";
-import properties from "@/properties.json";
+import { fetchProprties } from "@/services/apiProperties";
 
-export default function Properties() {
+export default async function Properties() {
+  const properties = await fetchProprties();
+
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
