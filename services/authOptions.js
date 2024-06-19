3938a1b96724d832814532ba08e303f8122e9735
kalema1 <kalemar8@gmail.com>
2024-06-19 15:55:58 +0300

    save user data on sign in
    - added logic in the  signIn function to save user data if it does not
      exist in the database.
    - added logic in the session function to assign session to the user.

--- services/authOptions.js
+++ services/authOptions.js
@@ -1,3 +1,6 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -18,16 +21,38 @@ export const authOptions = {
     //invoked on successfull signin
     async signIn({ profile }) {
       //1. connect to database
+      await connectDB();
+
       //2. check if user exists
+      const userExists = await User.findOne({ email: profile.email });
+
       //3. if not add user to database
+      if (!userExists) {
+        //truncate name if too long
+        const username = profile.name.slice(0, 20);
+
+        // save the user
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
+
       //4. return true to sign in user
+      return true;
     },
 
     // modifies the session
     async session({ session }) {
       //1. Get user from database
+      const user = await User.findOne({ email: session.user.email });
+
       //2. Assign the user id to the session
+      session.user.id = user._id.toString();
+
       //3. return session
+      return session;
     },
   },
 };
