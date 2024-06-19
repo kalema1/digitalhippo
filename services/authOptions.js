67b9b4b51a6b4d30a617f92ee8a64fac7412d477
kalema1 <kalemar8@gmail.com>
2024-06-19 11:49:52 +0300

    set up authentication fr the application
     - created the js file called authOption. For authentication options.
     - installed next-auth for our app authentication.
     - imported the GoogleProvider into the route file.
     - created the handle function from nextAuth.

---
+++ services/authOptions.js
@@ -0,0 +1,33 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authProviders = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    //invoked on successfull signin
+    async signIn({ profile }) {
+      //1. connect to database
+      //2. check if user exists
+      //3. if not add user to database
+      //4. return true to sign in user
+    },
+
+    // modifies the session
+    async session({ session }) {
+      //1. Get user from database
+      //2. Assign the user id to the session
+      //3. return session
+    },
+  },
+};
