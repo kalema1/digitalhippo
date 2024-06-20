7e27f393c1448587626b7c175530e497fd8de04f
kalema1 <kalemar8@gmail.com>
2024-06-20 23:47:03 +0300

    create useAddPrperty custom hook
    - created the useAddProperty custom hook for AddProperty component.
    - transfered all the logic to the hook..
    -imported the hook into the AddProperty compoenent.
    
    This ensures the separation of logic from the component. Making n good
    organisation and write of clean code.

--- components/property/AddProperty.jsx
+++ components/property/AddProperty.jsx
@@ -1,100 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useAddProperty } from "./useAddProperty";
 
 export default function AddProperty() {
-  const [fields, setFields] = useState({
-    type: "Apartment",
-    name: "Test Property",
-    description: "",
-    location: {
-      street: "",
-      city: "Test City",
-      state: "Test State",
-      zipcode: "",
-    },
-    beds: 3,
-    baths: 2,
-    square_feet: 1800,
-    amenities: [],
-    rates: {
-      weekly: "",
-      monthly: 2000,
-      nightly: "",
-    },
-    seller_info: {
-      name: "",
-      email: "teste@test.com",
-      phone: "",
-    },
-    images: [],
-  });
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-
-    // check if nested property
-    if (name.includes(".")) {
-      const [outerKey, innerKey] = name.split(".");
-
-      setFields((previousFields) => ({
-        ...previousFields,
-        [outerKey]: {
-          ...previousFields[outerKey],
-          [innerKey]: value,
-        },
-      }));
-    } else {
-      setFields((previousfields) => ({
-        ...previousfields,
-        [name]: value,
-      }));
-    }
-  }
-
-  function handleAmenitiesChange(event) {
-    const { value, checked } = event.target;
-
-    //clone the current array
-    const updatedAmenities = [...fields.amenities];
-
-    //check if the field is checked
-    if (checked) {
-      //add value to the array
-      updatedAmenities.push(value);
-    } else {
-      //remove the value from array
-      const index = updatedAmenities.indexOf(value);
-
-      if (index !== -1) {
-        updatedAmenities.splice(index, 1);
-      }
-    }
-
-    //update state with udated array
-    setFields((previousFields) => ({
-      ...previousFields,
-      amenities: updatedAmenities,
-    }));
-  }
-
-  function handleImageChange(event) {
-    const { files } = event.target;
-
-    //clone the images array
-    const updatedImages = [fields.images];
-
-    //add new files to the array
-    for (const file of files) {
-      updatedImages.push(file);
-    }
-
-    //pudate state with array of images
-    setFields((previousFields) => ({
-      ...previousFields,
-      images: updatedImages,
-    }));
-  }
+  const { handleAmenitiesChange, handleChange, handleImageChange, fields } =
+    useAddProperty();
 
   return (
     <section className="bg-blue-50">
