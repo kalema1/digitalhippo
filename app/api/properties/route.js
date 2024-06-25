b310752f187fe24f2b4e286e219cd9d72be3c50b
kalema1 <kalemar8@gmail.com>
2024-06-25 12:17:44 +0300

    upload images to cloudinary
    - created cloudinary.js file. To contain cloudinary configurations.
    - added logic in the api route to upload images to cloudinary.
    - added logic to save image url in the database.

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,6 +1,7 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { getSessionUser } from "@/services/getSessionUser";
+import cloudinary from "@/config/cloudinary";
 
 /*
  * gets /api/properties
@@ -75,6 +76,32 @@ export async function POST(request) {
       //images,
     };
 
+    //upload images to cloudinary
+    const imageUploadPromises = [];
+
+    for (const image of images) {
+      const imageBuffer = await image.arrayBuffer();
+      const imageArray = Array.from(new Uint8Array(imageBuffer));
+      const imageData = Buffer.from(imageArray);
+
+      // convert image data to base 64
+      const imageBase64 = imageData.toString("base64");
+
+      // make request to upload to cloudinary
+      const result = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        { folder: "propertypulse" }
+      );
+
+      imageUploadPromises.push(result.secure_url);
+
+      // wait for all images to upload
+      const uploadedImages = await Promise.all(imageUploadPromises);
+
+      // add uploaded images to propertydata object
+      propertyData.images = uploadedImages;
+    }
+
     const newProperty = new Property(propertyData);
     await newProperty.save();
 
@@ -82,6 +109,7 @@ export async function POST(request) {
       `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
     );
   } catch (error) {
+    console.log(error);
     return new Response("Failed to add Property", { status: 500 });
   }
 }
