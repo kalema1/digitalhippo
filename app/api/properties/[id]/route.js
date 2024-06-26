c1698b5e8a86bfc1e2aee974003892b00fa8bed5
kalema1 <kalemar8@gmail.com>
2024-06-26 19:44:04 +0300

    create a PUT request
    - created a PUT request in the api route. To update the property data.
    - added logic in the PUT request.
    - handled errors in the PUT request.

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -69,3 +69,79 @@ export async function DELETE(request, { params }) {
     return new Response("Something went wrong", { status: 500 });
   }
 }
+
+/*
+ * put data to the database
+ * PUT/api/properties/:id
+ */
+export async function PUT(request, { params }) {
+  try {
+    await connectDB();
+
+    const propertyId = params.id;
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    const formData = await request.formData();
+
+    //access all values from amenities
+    const amenities = formData.getAll("amenities");
+
+    // get proerty t update
+    const existingProperty = await Property.findById(propertyId);
+
+    if (!existingProperty) {
+      return new Response("Property does not exist", { status: 404 });
+    }
+
+    // verify ownership
+    if (existingProperty.owner.toString() !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    // create property object for the database
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly"),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      owner: userId,
+    };
+
+    // update property in database
+    const updatedProperty = await Property.findByIdAndUpdate(
+      propertyId,
+      propertyData
+    );
+
+    return new Response(JSON.stringify(updatedProperty), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed to add Property", { status: 500 });
+  }
+}
