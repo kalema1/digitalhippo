f9d1fb2121d44c194a037de71533e265fe735f03
kalema1 <kalemar8@gmail.com>
2024-06-27 14:43:42 +0300

    create the check api route
    - created the check api route. To be used to add bookmark checks.
    - added logic to the api POST request in the api check route. To handle
      the POST requests.

---
+++ app/api/bookmarks/check/route.js
@@ -0,0 +1,33 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import { getSessionUser } from "@/services/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(request) {
+  try {
+    await connectDB();
+    const { propertyId } = request.json();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // find user in database
+    const user = await User.findOne({ _id: userId });
+
+    // check if property is bookmarked
+    let isBookmarked = !!user.bookmarks.includes(propertyId);
+
+    return new Response(JSON.stringify({ isBookmarked }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something Went wrong", { status: 500 });
+  }
+}
