f04a457f50fb31065339cdaa56e8b01f81dc6529
kalema1 <kalemar8@gmail.com>
2024-06-27 11:48:23 +0300

    create  bookmarks api route request
    - created the bookmarks api route request. To handle the bookmarking of
      the property.
    - added logic to the api route.
    - performed error handling in the api route.

---
+++ app/api/bookmarks/route.js
@@ -0,0 +1,51 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/services/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(request) {
+  try {
+    await connectDB();
+    const { propertyId } = request.json();
+
+    const sessionUser = getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // find user in database
+
+    const user = await User.findOne({ _id: userId });
+
+    // check if property is bookmarked
+    let isBookmarked = !!user.bookmarks.includes(propertyId);
+
+    let message;
+
+    if (isBookmarked) {
+      // remove it if bookmarked
+      user.bookmarks.pull(propertyId);
+      message = "Bookmark removed Successfully";
+      isBookmarked = false;
+    } else {
+      // if not bookmarked, mark it
+      user.bookmarks.push(propertyId);
+      message = "Bookmark added Successfully";
+      isBookmarked = true;
+    }
+
+    await user.save();
+
+    return new Response(JSON.stringify({ message, isBookmarked }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something Went wrong", { status: 500 });
+  }
+}
