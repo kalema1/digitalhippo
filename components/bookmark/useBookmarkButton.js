12a92bf98a2dc17b759a75a6429054158060eb2d
kalema1 <kalemar8@gmail.com>
2024-06-27 15:24:09 +0300

    add useEffect to check bookmark
    - added a useEffect to check the bookmark.
    - bookmark button responds to the adding and removing of bookmark. State
      remains on page load.

--- components/bookmark/useBookmarkButton.js
+++ components/bookmark/useBookmarkButton.js
@@ -1,11 +1,34 @@
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 export function useBookmarkButton(property) {
   const [isBookmarked, setIsBookmarked] = useState(false);
   const { data: session } = useSession();
   const userId = session?.user?.id;
+
+  useEffect(() => {
+    async function checkBookmarkStatus() {
+      if (!userId) {
+        return;
+      }
+      try {
+        const response = await fetch("/api/bookmarks/check", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ propertyId: property._id }),
+        });
+
+        if (response.status === 200) {
+          const data = await response.json();
+          setIsBookmarked(data.isBookmarked);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    checkBookmarkStatus();
+  }, [property._id, userId]);
 
   /*
    * handles the click event on the bookmark button
