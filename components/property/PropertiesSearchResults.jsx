2a6e7c6bae97c40183bd4744499d1a5a158edcef
kalema1 <kalemar8@gmail.com>
2024-06-28 11:47:47 +0300

    add jsx to PropertiesSearchResults component
    - added jsx ro the component to render the searched properties.
    - added logic to render the searched properties.

--- components/property/PropertiesSearchResults.jsx
+++ components/property/PropertiesSearchResults.jsx
@@ -1,8 +1,28 @@
 "use client";
 
+import Loader from "@/ui/Loader";
 import { usePropertiesSearchResults } from "./usePropertiesSearchResults";
+import PropertyCard from "../propertyCard/PropertyCard";
+import BackButton from "@/ui/BackButton";
 
 export default function PropertiesSearchResults() {
-  const {} = usePropertiesSearchResults();
-  return <div>search reslts </div>;
+  const { properties, isLoading } = usePropertiesSearchResults();
+
+  if (isLoading) {
+    return <Loader loading={isLoading} />;
+  }
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <BackButton />
+        <h1 className="text-2xl mb-4">Search Results</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {properties.length === 0 && <div>No Search Results Found</div>}
+          {properties.map((property) => (
+            <PropertyCard key={property._id} property={property} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
