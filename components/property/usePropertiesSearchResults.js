a1e435f2de14fb664869a55549751e96f8359912
kalema1 <kalemar8@gmail.com>
2024-06-28 11:27:52 +0300

    add logic in the usePropertiesSearchResults hook
    - created a useFfect to fetch searched properties.
    - added error handling in the useEffect.
    
    This update ensures searched properties are being fetched from the api
    endpoint.

--- components/property/usePropertiesSearchResults.js
+++ components/property/usePropertiesSearchResults.js
@@ -1,5 +1,5 @@
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function usePropertiesSearchResults() {
   const [properties, setProperties] = useState([]);
@@ -9,4 +9,28 @@ export function usePropertiesSearchResults() {
 
   const location = searchParams.get("location");
   const propertyType = searchParams.get("propertyType");
+
+  useEffect(() => {
+    async function fetchSearchedProperties() {
+      try {
+        const response = await fetch(
+          `/api/properties/search?location=${location}&propertyType=${propertyType}`
+        );
+
+        if (response.status !== 200) {
+          return;
+        }
+
+        const data = await response.json();
+        setProperties(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchSearchedProperties();
+  }, [location, propertyType]);
+
+  return { properties, isLoading };
 }
