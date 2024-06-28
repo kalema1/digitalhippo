b158c937e3416c9f44ea9b6d2eb0cfd188ae8460
kalema1 <kalemar8@gmail.com>
2024-06-28 12:04:46 +0300

    create a search section component
    - created a SearchFormSection component. To render the section
      containing the search form.
    - added the form component.
    - added the styles for the section.
    - imported the component into the PropertiesSearchResults component.
    
    This update ensures reuseability of the SearchFormSection in diffierent
    pages.

--- components/property/PropertiesSearchResults.jsx
+++ components/property/PropertiesSearchResults.jsx
@@ -4,6 +4,7 @@ import Loader from "@/ui/Loader";
 import { usePropertiesSearchResults } from "./usePropertiesSearchResults";
 import PropertyCard from "../propertyCard/PropertyCard";
 import BackButton from "@/ui/BackButton";
+import SearchFormSection from "@/ui/SearchFormSection";
 
 export default function PropertiesSearchResults() {
   const { properties, isLoading } = usePropertiesSearchResults();
@@ -12,17 +13,20 @@ export default function PropertiesSearchResults() {
     return <Loader loading={isLoading} />;
   }
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        <BackButton />
-        <h1 className="text-2xl mb-4">Search Results</h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {properties.length === 0 && <div>No Search Results Found</div>}
-          {properties.map((property) => (
-            <PropertyCard key={property._id} property={property} />
-          ))}
+    <>
+      <SearchFormSection />
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <BackButton />
+          <h1 className="text-2xl mb-4">Search Results</h1>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.length === 0 && <div>No Search Results Found</div>}
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
---
+++ ui/SearchFormSection.jsx
@@ -0,0 +1,11 @@
+import PropertySearchForm from "@/components/property/PropertySearchForm";
+
+export default function SearchFormSection() {
+  return (
+    <section className="bg-blue-700 py-4">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+        <PropertySearchForm />
+      </div>
+    </section>
+  );
+}
