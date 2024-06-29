d6014445d92ba5f37a244944ddc073ede9d33e1b
kalema1 <kalemar8@gmail.com>
2024-06-29 14:35:53 +0300

    add jsx
    - added jsx to the Messages component.
    - created the Message component. To contain single message information.
    - added jsx to the Message component.
    - imported the Message component into the Messages component.

--- components/messages/Messages.jsx
+++ components/messages/Messages.jsx
@@ -1,5 +1,16 @@
 "use client";
 
+import Message from "./Message";
+
 export default function Messages() {
-  return <div>messages</div>;
+  return (
+    <section className="bg-blue-50">
+      <div className="container m-auto py-24 max-w-6xl">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
+          <Message />
+        </div>
+      </div>
+    </section>
+  );
 }
