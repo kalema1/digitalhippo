c022c1883dcf31460e5de007e0473006fb5dc3ef
kalema1 <kalemar8@gmail.com>
2024-07-12 20:56:14 +0300

    fix: fix styles for the search results on the mobile browser
    - added style for the elements in the PropertiesSearchResults component.
      To show the text on the mobile browser.

--- components/property/PropertiesSearchResults.jsx
+++ components/property/PropertiesSearchResults.jsx
@@ -18,9 +18,11 @@ export default function PropertiesSearchResults() {
       <section className="px-4 py-6">
         <div className="container-xl lg:container m-auto px-4 py-6">
           <BackButton />
-          <h1 className="text-2xl mb-4">Search Results</h1>
+          <h1 className="text-2xl mb-4 text-gray-800">Search Results</h1>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.length === 0 && <div>No Search Results Found</div>}
+            {properties.length === 0 && (
+              <div className="text-gray-800">No Search Results Found</div>
+            )}
             {properties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
